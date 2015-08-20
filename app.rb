d9a7e179572e1822d8351b4e5ede69b5acba9ad1
Ryan Broughan & Kyle Mellander <student@epicodus.com>
2015-08-20 09:56:41 -0700

    Add 'Add a book' functionality for books with 1 author not already added into the database

--- app.rb
+++ app.rb
@@ -12,3 +12,18 @@ get '/' do
   @books = Book.all
   erb(:index)
 end
+
+get '/book/add' do
+  erb(:add_book)
+end
+
+post '/book/submit' do
+  title = params.fetch('title')
+  author = params.fetch('author')
+  book_id = Book.new({title: title}).save.to_i
+  author_id = Author.new({name: author}).save.to_i
+  book = Book.find(book_id)
+  book.update({author_ids: [author_id]})
+  @success_message = "You have added #{title} by #{author}."
+  erb(:add_book)
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'capybara/rspec'
 require './app'
+
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
@@ -8,5 +9,19 @@ describe('/', {:type => :feature}) do
   it 'displays index page with books' do
     visit('/')
     expect(page).to have_content("Library")
+  end
+end
+
+describe('book/add', {:type => :feature}) do
+  it 'displays index page with books' do
+    visit('/book/add')
+    fill_in "title", with: "The Foundation"
+    fill_in "author", with: "Isaac Asimov"
+    click_button "Add Book"
+    expect(page).to have_content("The Foundation")
+    expect(page).to have_content("Isaac Asimov")
+    visit('/')
+    expect(page).to have_content("The Foundation")
+    expect(page).to have_content("Isaac Asimov")
   end
 end
